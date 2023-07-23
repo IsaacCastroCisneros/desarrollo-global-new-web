be1ea19f97a556ef4aa68ebe4a0cb805764ae10a
isaac <isaaccastrocisneros@gmail.com>
2023-07-23 14:28:28 -0500

    no buttons

--- components/NavBar/Navbar.tsx
+++ components/NavBar/Navbar.tsx
@@ -40,14 +40,14 @@ export default function Navbar()
             <Item key={pos} href={item.href} label={item.label} />
           ))}
         </ul>
-        <Button name='ingresar al aula virtual' className="ml-auto navbar1:hidden">ingreso aula virtual</Button>
+       {/*  <Button name='ingresar al aula virtual' className="ml-auto navbar1:hidden">ingreso aula virtual</Button>
         <button
           className="navbar:block hidden ml-[1rem]"
           onClick={() => setShow(!show)}
           name='abrir menu'
         >
           <FontAwesomeIcon size="xl" icon={faBars} />
-        </button>
+        </button> */}
       </nav>
     </header>
   );
