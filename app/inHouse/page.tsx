200ec6738ac3642136b72f248b5711eb5945c512
isaac <isaaccastrocisneros@gmail.com>
2023-07-24 18:25:54 -0500

    inhouuuse

--- app/inHouse/page.tsx
+++ app/inHouse/page.tsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero/Hero'
 import StripeInHouse from './components/StripeInHouse'
 import CatalogoDeProgramas from './components/CatalogoDeProgramas/CatalogoDeProgramas'
 import PorQueElegirUnCursoInhouse from './components/PorQueElegirUnCursoInhouse/PorQueElegirUnCursoInhouse'
+import DescubraComoPodemosAyudarle from './components/DescubraComoPodemosAyudarle/DescubraComoPodemosAyudarle'
 
 export default function page() {
   return(
